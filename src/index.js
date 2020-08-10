55add92713dc3dab500a7952726f1bbb64962bc6
bera5186 <rbasu611@gmail.com>
2020-08-11 01:33:16 +0530

    Configured tasks, finally wrote some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function() {
+const fs = require('fs')
+
+function readFileBinod(file) {
     console.log('Binod *_* !!')
+
+    fs.readFile(file, 'utf8', (err, data) => { 
+        if (err) throw err;   
+        
+        if(data.includes('binod')) {
+            console.log('Binod Found in file')
+        }
+    }); 
+    
 }
+module.exports = readFileBinod
